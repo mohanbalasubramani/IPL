d4a4bac1cf0ec586b08a4c7f3f61e0e6f0687232
Mohan Balasubramani <mohan_krish18@yahoo.in>
2020-03-09 21:32:30 +0530

    initial commit

---
+++ precache-manifest.6052023daec8dbfc60dc9f1ee04cf34b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb601870d29e6fef80518a9e92f9394",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f2b8d9c19f3444e338d",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c9218b67bafb4f37259e",
+    "url": "/static/css/main.d5c8a757.chunk.css"
+  },
+  {
+    "revision": "3f2b8d9c19f3444e338d",
+    "url": "/static/js/2.69abcdb8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.69abcdb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9218b67bafb4f37259e",
+    "url": "/static/js/main.4b9b4183.chunk.js"
+  },
+  {
+    "revision": "09fe3b13ac62afa26121",
+    "url": "/static/js/runtime-main.687138c8.js"
+  }
+]);
